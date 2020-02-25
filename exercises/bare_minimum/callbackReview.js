cec3880eb504a0e3e58b50dc54420efc7dc01a3e
jsim0809 <jeremy_sim@hotmail.com>
2020-02-25 14:27:49 -0800

    Complete pluckFirstLineFromFile

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,8 +6,26 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, 'utf8', (err, text) => {
+    if (err) {
+      callback(err, null);
+    } else {
+      var splitText = text.split('\n');
+      callback(null, splitText[0]);
+    }
+  });
+
+  /*fileStream.on('data', function(data) {
+    fileData += data;
+
+    var lines = fileData.split('\n');
+
+    callback(null, lines[0]);
+
+  });*/
+
+
 };
 
 // This function should retrieve the status code of a GET request to `url`
